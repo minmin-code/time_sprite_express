6ef0649810d3e58e9a559714cad68bdd1139cc15
mia-family <wm66400@163.com>
2022-07-20 18:59:06 +0800

    feat:新增接口

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 /*
  * @Author: 王敏
- * @LastEditTime: 2022-07-18 17:50:12
+ * @LastEditTime: 2022-07-20 18:58:54
  * @Description: file content
  */
 import { PrismaClient } from '@prisma/client'
@@ -13,24 +13,10 @@ const prisma = new PrismaClient()
 
 app.use(cors({
   origin: 'http://localhost:8000', //前端地址
-  methods: ['GET', 'POST'],
+  methods: ['GET', 'POST','PUT','DELETE'],
   alloweHeaders: ['Conten-Type', 'Authorization'],
   credentials: true
 }))
-// app.use((req:any, res:any, next:Function) => {
-//   // 即在不同域名下发出的请求也可以携带 cookie
-//   res.header("Access-Control-Allow-Credentials",true)
-//   // 第二个参数表示允许跨域的域名，* 代表所有域名
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:8000')//配置80端口跨域
-//   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS') // 允许的 http 请求的方法
-//   // 允许前台获得的除 Cache-Control、Content-Language、Content-Type、Expires、Last-Modified、Pragma 这几张基本响应头之外的响应头
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-//   if (req.method == 'OPTIONS') {
-//       res.sendStatus(200)
-//   } else {
-//       next()
-//   }
-// })
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
@@ -92,9 +78,7 @@ interface TaskType {
   id?: string
   title: string // 标题
   subTitle?: string // 副标
-  dateType?: number // 时间类型
-  date?: string // 具体日期
-  clock?: string[] // 闹钟 （具体时间时，分）
+  ddl?: string // 截至日期
   taskList: TaskType[] // 任务列表
 }
 
@@ -102,13 +86,12 @@ interface TaskType {
 /**
  * 任务列表
  */
+//新增任务
 app.post('/home/addTask', async (req:any, res:any) => {
   let {
     title, // 标题
     subTitle, // 副标
-    dateType, // 时间类型
-    date, // 具体日期
-    clock, // 闹钟 （具体时间时，分）
+    ddl,
     taskList // 任务列表
   }:AddFormType=req.body
   try{
@@ -116,10 +99,7 @@ app.post('/home/addTask', async (req:any, res:any) => {
       data: {
         title, // 标题
         subTitle, // 副标
-        dateType, // 时间类型
-        date, // 具体日期
-        clock: clock?.toString(), // 闹钟 （具体时间时，分）
-        // taskList, // 任务列表
+        ddl,
         taskList: {
           create: taskList
         }
@@ -132,14 +112,36 @@ app.post('/home/addTask', async (req:any, res:any) => {
     res.send({code:500,message:'添加失败'})
   }
 })
-
+//获取任务列表
 app.get('/home/getTodoList', async (req:any, res:any) => {
   let {
-    date
-  }=req.body
+    ddl
+  }=req.query
   const data = await prisma.todo.findMany({
     where:{
-      date
+      // ddl
+      ddl:{
+        startsWith:ddl
+      }
+    },
+    include: {
+      taskList: true
+    }
+  })
+  res.send({code:200,data:data})
+})
+
+app.post('/home/updateSubTask', async (req:any, res:any) => {
+  let {
+    id,
+    ...rest
+  }=req.query
+  const data = await prisma.todo.update({
+    where:{
+      id
+    },
+    data:{
+      ...rest
     },
     include: {
       taskList: true
